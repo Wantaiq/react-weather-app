03dd63f0eed2df00df5a54a03e9ed50263ade214
Filip <filipdopa@yahoo.com>
2022-05-12 19:10:36 +0200

    Implement geolocation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { useEffect, useState } from 'react';
+
 function App() {
-  return <h1>hello</h1>;
+  const [location, setLocation] = useState('');
+
+  useEffect(() => {
+    if (!location) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLocation({
+            longitude: position.coords.longitude,
+            latitude: position.coords.latitude,
+          });
+        },
+        (error) => {
+          if (error.code === error.PERMISSION_DENIED) {
+            setLocation('');
+          }
+        },
+      );
+    }
+  });
+
+  return <h1>Weather App</h1>;
 }
 
 export default App;
